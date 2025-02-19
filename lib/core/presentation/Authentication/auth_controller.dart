7a24b6fdebef74b6117983aa3b0d9208ceb4b2af
ChristopherV654 <144166989+ChristopherV654@users.noreply.github.com>
2025-02-19 15:12:55 -0500

    Feat: Se cambia de package para verificacion de conexion a internet para Android e iOS

--- lib/core/presentation/Authentication/auth_controller.dart
+++ lib/core/presentation/Authentication/auth_controller.dart
@@ -1,18 +1,20 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:identity_engine/core/Routes/router.dart';
 import 'package:identity_engine/core/presentation/widget/widgets.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:local_auth/local_auth.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
 
 class AuthController extends GetxController {
   final LocalAuthentication auth = LocalAuthentication();
   RxBool isAuthenticated = false.obs;
   RxBool isConnected = false.obs;
   RxBool isCheckingConnection = true.obs;
+
+  StreamSubscription<InternetConnectionStatus>? connectionSubscription;
+
+  // late StreamSubscription _connectionSubscription;
 
   @override
   void onInit() {
@@ -21,30 +23,52 @@ class AuthController extends GetxController {
   }
 
   @override
-  void dispose() {
-    subscription.cancel();
-    super.dispose();
+  void onClose() {
+    connectionSubscription!.cancel(); // Cancelar el listener
+    super.onClose();
   }
 
   Future<void> checkConnectivity() async {
     isCheckingConnection.value = true;
     showCheckingConnectionDialog();
-    await Future.delayed(
-      const Duration(seconds: 3),
-    );
-    var connectivityResult = await (Connectivity().checkConnectivity());
+
+    bool hasConnection =
+        await InternetConnectionChecker.createInstance().hasConnection;
     isCheckingConnection.value = false;
-    Get.back();
-    if (connectivityResult.contains(ConnectivityResult.mobile) ||
-        connectivityResult.contains(ConnectivityResult.wifi)) {
+    Get.back(); // Cierra el diálogo de carga
+
+    if (hasConnection) {
       isConnected.value = true;
-      Get.offNamed(Routes.home);
     } else {
       isConnected.value = false;
       showNoConnectionDialog();
     }
+
+    // Inicia el listener para cambios de conexión
+    startListeningConnectionChanges();
   }
 
+  void startListeningConnectionChanges() {
+    // Cancela cualquier listener anterior (si lo hay) para evitar duplicados
+    // connectionSubscription?.cancel();
+
+    connectionSubscription = InternetConnectionChecker.createInstance()
+        .onStatusChange
+        .listen((status) {
+      if (status == InternetConnectionStatus.connected) {
+        if (isConnected.value) {
+          Get.offNamed(
+              Routes.home); // Solo redirecciona si se desconectó previamente
+        }
+      } else if (status == InternetConnectionStatus.disconnected) {
+        if (isConnected.value) {
+          isConnected.value = false;
+          showNoConnectionDialog(); // Muestra el diálogo si pierde conexión
+        }
+      }
+    });
+    // connectionSubscription?.cancel();
+  }
   Future<void> authenticate() async {
     try {
       bool canCheckBiometrics = await auth.canCheckBiometrics;
@@ -68,10 +92,9 @@ class AuthController extends GetxController {
       isAuthenticated.value = true;
     }
 
-    // Después de completar la autenticación, navegar a la siguiente pantalla
     if (isAuthenticated.value) {
-      // checkConnectivity();
-      getConnectivity();
+      checkConnectivity();
+      // startConnectionListener(); // Iniciar el listener de conexión
     }
   }
 
@@ -98,43 +121,14 @@ class AuthController extends GetxController {
   }
 
   Future<void> showNoConnectionDialog() async {
-    // if (Get.isDialogOpen == false) {
-      Get.dialog(
-        barrierDismissible: false,
-        CustomDialogLostConection(
-          onRetry: () async {
-            Get.back(); // Cierra el diálogo
-            await checkInternetAndNavigate();
-          },
-        ),
-      );
-    // }
-  }
-
-  //! NEW CONNECTION METHOD
-
-  late StreamSubscription subscription;
-  var isDeviceConnected = false.obs;
-  RxBool isAlertSet = false.obs;
-
-  Future<void> checkInternetAndNavigate() async {
-    showCheckingConnectionDialog();
-    isDeviceConnected.value =
-        await InternetConnectionChecker.createInstance().hasConnection;
-    if (isDeviceConnected.value) {
-      Get.back(); // Cierra el diálogo de verificación
-      Get.offNamed(Routes.home);
-    } else {
-      Get.back(); // Cierra el diálogo de verificación
-      showNoConnectionDialog();
-    }
-  }
-
-  void getConnectivity() {
-    subscription = Connectivity().onConnectivityChanged.listen(
-      (List<ConnectivityResult> result) async {
-        await checkInternetAndNavigate();
-      },
+    Get.dialog(
+      barrierDismissible: false,
+      CustomDialogLostConection(
+        onRetry: () async {
+          Get.back(); // Cierra el diálogo
+          await checkConnectivity(); // Reintenta la verificación
+        },
+      ),
     );
   }
 }
--- lib/core/presentation/home/home_screen.dart
+++ lib/core/presentation/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:identity_engine/core/Styles/app_colors.dart';
 import 'package:identity_engine/core/presentation/home/home_controller.dart';
@@ -8,7 +9,7 @@ import 'package:identity_engine/core/presentation/pages/identities/identities_sc
 import 'package:identity_engine/core/presentation/pages/scanner_qr/scanner_screen.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
-class HomeScreen extends GetWidget<HomeController>{
+class HomeScreen extends GetWidget<HomeController> {
   const HomeScreen({super.key});
 
   @override
@@ -16,106 +17,121 @@ class HomeScreen extends GetWidget<HomeController>{
     final IdentitiesController identitiesController = Get.find();
 
     return Obx(
-      () => Scaffold(
-        appBar: AppBar(
-          title: (controller.isSearching.value &&
-                  controller.currentPage.value == 0)
-              ? TextField(
-                  controller: controller.searchController,
-                  autofocus: true,
-                  style: const TextStyle(
-                    color: AppColors.white,
-                  ),
-                  decoration: const InputDecoration(
-                    hintText: 'Buscar...',
-                    border: InputBorder.none,
-                    hintStyle: TextStyle(
+      () => PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (didPop, __) {
+          if (!didPop) {
+            Fluttertoast.showToast(
+              msg: 'En esta pantalla no se puede usar el botón de retroceso',
+              timeInSecForIosWeb: 5,
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              backgroundColor: Colors.grey.shade800,
+              textColor: Colors.white,
+              fontSize: 16.0,
+            );
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: (controller.isSearching.value &&
+                    controller.currentPage.value == 0)
+                ? TextField(
+                    controller: controller.searchController,
+                    autofocus: true,
+                    style: const TextStyle(
                       color: AppColors.white,
                     ),
-                  ),
-                  onChanged: (query) {
-                    identitiesController.filterIdentities(query);
-                  },
-                )
-              : const Text(
-                  'Mardis Identity',
-                  style: TextStyle(
-                    color: AppColors.white,
-                  ),
-                ),
-          leading: (controller.isSearching.value &&
-                  controller.currentPage.value == 0)
-              ? IconButton(
-                  icon: const Icon(
-                    Icons.arrow_back,
-                    color: AppColors.white,
-                  ),
-                  onPressed: controller.stopSearch,
-                )
-              : null,
-          actions: (controller.isSearching.value &&
-                  controller.currentPage.value == 0)
-              ? [
-                  IconButton(
-                    icon: const Icon(
-                      Icons.clear,
-                      color: AppColors.white,
-                    ),
-                    onPressed: () {
-                      controller.searchController.clear();
-                      identitiesController.filterIdentities('');
-                    },
-                  ),
-                ]
-              : [
-                  if (controller.currentPage.value == 0)
-                    IconButton(
-                      icon: const Icon(
-                        Icons.search,
+                    decoration: const InputDecoration(
+                      hintText: 'Buscar...',
+                      border: InputBorder.none,
+                      hintStyle: TextStyle(
                         color: AppColors.white,
                       ),
-                      onPressed: controller.startSearch,
-                      
-                    )
-                ],
-        ),
-        body: PageView(
-          onPageChanged: controller.animateToTab,
-          controller: controller.pageController,
-          physics: const BouncingScrollPhysics(),
-          children: const [
-            IdentitiesScreen(),
-            ScannerScreen(),
-            ConfigurationScreen(),
-          ],
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: AppColors.white,
-          notchMargin: 10,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _bottomAppBarItem(
-                context,
-                icon: Icons.fingerprint_rounded,
-                page: 0,
-                label: 'Identidades',
-              ),
-              _bottomAppBarItem(
-                context,
-                icon: Icons.qr_code_scanner_rounded,
-                page: 1,
-                label: 'Escaneo de QR',
-              ),
-              _bottomAppBarItem(
-                context,
-                icon: Icons.perm_device_information_rounded,
-                page: 2,
-                label: 'Información',
-              ),
+                    ),
+                    onChanged: (query) {
+                      identitiesController.filterIdentities(query);
+                    },
+                  )
+                : const Text(
+                    'Mardis Identity',
+                    style: TextStyle(
+                      color: AppColors.white,
+                    ),
+                  ),
+            leading: (controller.isSearching.value &&
+                    controller.currentPage.value == 0)
+                ? IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: AppColors.white,
+                    ),
+                    onPressed: controller.stopSearch,
+                  )
+                : null,
+            actions: (controller.isSearching.value &&
+                    controller.currentPage.value == 0)
+                ? [
+                    IconButton(
+                      icon: const Icon(
+                        Icons.clear,
+                        color: AppColors.white,
+                      ),
+                      onPressed: () {
+                        controller.searchController.clear();
+                        identitiesController.filterIdentities('');
+                      },
+                    ),
+                  ]
+                : [
+                    if (controller.currentPage.value == 0)
+                      IconButton(
+                        icon: const Icon(
+                          Icons.search,
+                          color: AppColors.white,
+                        ),
+                        onPressed: controller.startSearch,
+                      )
+                  ],
+          ),
+          body: PageView(
+            onPageChanged: controller.animateToTab,
+            controller: controller.pageController,
+            physics: const BouncingScrollPhysics(),
+            children: const [
+              IdentitiesScreen(),
+              ScannerScreen(),
+              ConfigurationScreen(),
             ],
           ),
-          // ),
+          bottomNavigationBar: BottomAppBar(
+            color: AppColors.white,
+            notchMargin: 10,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _bottomAppBarItem(
+                  context,
+                  icon: Icons.fingerprint_rounded,
+                  page: 0,
+                  label: 'Identidades',
+                ),
+                _bottomAppBarItem(
+                  context,
+                  icon: Icons.qr_code_scanner_rounded,
+                  page: 1,
+                  label: 'Escaneo de QR',
+                ),
+                _bottomAppBarItem(
+                  context,
+                  icon: Icons.perm_device_information_rounded,
+                  page: 2,
+                  label: 'Información',
+                ),
+              ],
+            ),
+            // ),
+          ),
         ),
       ),
     );
@@ -132,12 +148,10 @@ class HomeScreen extends GetWidget<HomeController>{
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Icon(
-            icon,
-            color: controller.currentPage.value == page
-                ? AppColors.red
-                : AppColors.grey
-          ),
+          Icon(icon,
+              color: controller.currentPage.value == page
+                  ? AppColors.red
+                  : AppColors.grey),
           Text(
             label,
             style: TextStyle(
